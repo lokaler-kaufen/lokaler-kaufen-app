a3857ce85cd05582bcc1242b03761730fa7ccdb6
Moritz Kammerer <moritz.kammerer@qaware.de>
2020-03-20 23:45:26 +0100

    Add JavaDoc for Franz

--- backend/src/main/java/de/qaware/mercury/mercury/util/Lists.java
+++ backend/src/main/java/de/qaware/mercury/mercury/util/Lists.java
@@ -9,6 +9,15 @@ public final class Lists {
     private Lists() {
     }
 
+    /**
+     * Lists a collection of type 'input' to a list of type 'output' using a mapper function.
+     *
+     * @param in     list of type 'input'
+     * @param mapper maps from 'input' to 'output'
+     * @param <I>    input type
+     * @param <O>    output type
+     * @return list of type 'output'
+     */
     public static <I, O> List<O> map(Collection<I> in, Function<I, O> mapper) {
         List<O> result = new ArrayList<>(in.size());
 
