c804055844c1807c9f9e226277915b685f7ee519
Roland Hummel <roland.hummel@qaware.de>
2020-03-20 23:37:40 +0100

    Frontend: Created bare footer

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { ShopSearchPageComponent } from './shop-search-page/shop-search-page.com
 import { ShopDetailsPageComponent } from './shop-details-page/shop-details-page.component';
 import { ShopCreationPageComponent } from './shop-creation-page/shop-creation-page.component';
 import { ShopManagementPageComponent } from './shop-management-page/shop-management-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ImprintPageComponent } from './imprint-page/imprint-page.component';
+import { PrivacyPageComponent } from './privacy-page/privacy-page.component';
 
 const routes : Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'shops', component: ShopSearchPageComponent},
   {path: 'shops/:id', component: ShopDetailsPageComponent},
   {path: 'create-shop', component: ShopCreationPageComponent},
-  {path: 'manage-shop', component: ShopManagementPageComponent}
+  {path: 'manage-shop', component: ShopManagementPageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'imprint', component: ImprintPageComponent},
+  {path: 'privacy', component: PrivacyPageComponent}
 ];
 
 @NgModule({
