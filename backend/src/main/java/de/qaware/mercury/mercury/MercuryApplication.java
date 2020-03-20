3057471395130c3e81c61d3e8ddc518adfe5732c
Moritz Kammerer <moritz.kammerer@qaware.de>
2020-03-20 22:30:56 +0100

    Add spring boot project

---
+++ backend/src/main/java/de/qaware/mercury/mercury/MercuryApplication.java
@@ -0,0 +1,11 @@
+package de.qaware.mercury.mercury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MercuryApplication.class, args);
+    }
+}
