0ddd00d85c5d2d40109813c93bccb90d4840826c
Felix Kelm <felix.kelm@qaware.de>
2020-03-21 10:00:59 +0100

    adding location (draft) * lookup to integrade * sql query to implement

--- backend/src/main/resources/db/migration/V1__add_shop_table.sql
+++ backend/src/main/resources/db/migration/V1__add_shop_table.sql
@@ -2,5 +2,8 @@ CREATE TABLE shop
 (
     id   uuid primary key,
     name text not null,
-    enabled boolean not null
+    enabled boolean not null,
+    location_name text not null,
+    latitude double not null,
+    longitude double not null
 );
