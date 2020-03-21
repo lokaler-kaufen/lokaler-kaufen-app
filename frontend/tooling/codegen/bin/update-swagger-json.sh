29010d0e468e516ab67e165ac1c839349333846a
Uli Schweikl <extern.schweikl_uli@allianz.de>
2020-03-21 23:49:18 +0100

    Do some manual tweaking within the swagger.json. #25

--- frontend/tooling/codegen/bin/update-swagger-json.sh
+++ frontend/tooling/codegen/bin/update-swagger-json.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 rm ../swagger.json
-curl http://localhost:8080/v2/api-docs > ../swagger.json
+curl http://localhost:8080/v2/api-docs > ../swagger_original.json
+
+# Set the actual local frontend host
+sed 's/localhost:8080/localhost:4200/' ../swagger_original.json > ../swagger.json
 
 echo "done"
