2148dd15b878de5820d58f207add30121bc0fe41
Franz Wimmer <franz.wimmer@qaware.de>
2020-03-21 16:46:35 +0100

    ContactType enum only uses english now

---
+++ backend/src/main/java/de/qaware/mercury/mercury/storage/shop/ContactType.java
@@ -0,0 +1,27 @@
+package de.qaware.mercury.mercury.storage.shop;
+
+public enum ContactType {
+
+    FACEBOOK_MESSENGER("FACEBOOK_MESSENGER"),
+    GLIDE("GLIDE"),
+    GOOGLE_DUO("GOOGLE_DUO"),
+    WHATSAPP("WHATSAPP"),
+    SKYPE("SKYPE"),
+    JUSTALK("JUSTALK"),
+    SIGNAL_PRIVATE_MESSENGER("SIGNAL_PRIVATE_MESSENGER"),
+    SNAPCHAT("SNAPCHAT"),
+    TANGO("TANGO"),
+    VIBER("VIBER"),
+    TELEPHONE("TELEPHONE");
+
+    private final String value;
+
+    ContactType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
