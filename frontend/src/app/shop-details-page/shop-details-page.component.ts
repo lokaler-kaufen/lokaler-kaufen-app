ad489b47fc3c1ad96e4595b459ee4d212b33780b
Lukas Weber <lukas.b.weber@student.hs-rm.de>
2020-03-21 14:32:45 +0100

    Open booking dialogue from detail page #6

--- frontend/src/app/shop-details-page/shop-details-page.component.ts
+++ frontend/src/app/shop-details-page/shop-details-page.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {BookingPopupComponent, BookingPopupOutcome, BookingPopupResult} from '../booking-popup/booking-popup.component';
 
 class ShopDetails {
   id: string;
@@ -32,7 +34,8 @@ export class ShopDetailsPageComponent implements OnInit {
 
 
   constructor(private httpClient: HttpClient,
-              private activatedRoute: ActivatedRoute) {
+              private activatedRoute: ActivatedRoute,
+              private matDialog: MatDialog) {
   }
 
   ngOnInit(): void {
@@ -59,31 +62,37 @@ export class ShopDetailsPageComponent implements OnInit {
       website: 'https://www.moesdrinks.com',
       slots: [
         {
+          id: '0',
           start: '10:00',
           end: '11:00',
           available: false
         },
         {
+          id: '1',
           start: '11:00',
           end: '12:00',
           available: false
         },
         {
+          id: '2',
           start: '12:00',
           end: '13:00',
           available: false
         },
         {
+          id: '3',
           start: '13:00',
           end: '14:00',
           available: true
         },
         {
+          id: '4',
           start: '14:00',
           end: '15:00',
           available: true
         },
         {
+          id: '5',
           start: '15:00',
           end: '16:00',
           available: true
@@ -96,5 +105,20 @@ export class ShopDetailsPageComponent implements OnInit {
       });
   }
 
-  showBookingPopup(id: string) {}
+  showBookingPopup(id: string) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '100%';
+
+    this.matDialog.open(BookingPopupComponent, dialogConfig)
+      .afterClosed()
+      .subscribe((data: BookingPopupResult) => {
+        if (data.outcome === BookingPopupOutcome.BOOK) {
+          // TODO http call to the frontend api goes here
+          console.warn('NOT IMPLEMENTED', data, id);
+        }
+      });
+  }
+
+
 }
