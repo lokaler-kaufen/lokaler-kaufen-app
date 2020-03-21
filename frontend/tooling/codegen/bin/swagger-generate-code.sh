2b33f045dfbcb5181739a07f1939bec81200d0a9
Uli Schweikl <extern.schweikl_uli@allianz.de>
2020-03-21 21:48:07 +0100

    Generate the backend client code via swagger codegen. #25

---
+++ frontend/tooling/codegen/bin/swagger-generate-code.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+SCRIPT="$0"
+
+while [ -h "$SCRIPT" ] ; do
+  ls=`ls -ld "$SCRIPT"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    SCRIPT="$link"
+  else
+    SCRIPT=`dirname "$SCRIPT"`/"$link"
+  fi
+done
+
+if [ ! -d "${APP_DIR}" ]; then
+  APP_DIR=`dirname "$SCRIPT"`/..
+  APP_DIR=`cd "${APP_DIR}"; pwd`
+fi
+
+executable="./swagger-codegen-cli.jar"
+
+# if you've executed sbt assembly previously it will use that instead.
+export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -Dlogback.configurationFile=bin/logback.xml"
+
+echo "Mercury API client"
+ags="$@ generate -i ../swagger.json -l typescript-angular -o ../../../src/app/data/client/ --additional-properties ngVersion=9"
+java $JAVA_OPTS -jar $executable $ags
