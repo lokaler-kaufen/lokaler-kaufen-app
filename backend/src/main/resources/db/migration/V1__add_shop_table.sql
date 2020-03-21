78fdbae630230de2c6bcea97e97c37b8032302f4
Moritz Kammerer <moritz.kammerer@qaware.de>
2020-03-21 10:43:32 +0100

    Reformat SQL

--- backend/src/main/resources/db/migration/V1__add_shop_table.sql
+++ backend/src/main/resources/db/migration/V1__add_shop_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE shop
 (
-    id   uuid primary key,
-    name text not null,
-    enabled boolean not null,
-    location_name text not null,
-    latitude double not null,
-    longitude double not null
+    id            uuid primary key,
+    name          text    not null,
+    enabled       boolean not null,
+    location_name text    not null,
+    latitude      double  not null,
+    longitude     double  not null
 );
