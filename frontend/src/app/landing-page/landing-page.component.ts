3b341e6b27814c7cb3dcf485e7de18454ec40285
Veronika Dashuber <veronika.dashuber@qaware.de>
2020-03-21 15:15:27 +0100

    [FRONTEND] Landing page: make location input required

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
+  location: string;
 
   constructor() { }
 
