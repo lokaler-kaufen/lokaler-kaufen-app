f84618adf24b448e04e796aa30ede886a8814417
Uli Schweikl <extern.schweikl_uli@allianz.de>
2020-03-22 16:20:41 +0100

    A login form. #35

--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'login-page',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  formController = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    const formValue: any = this.formController.value;
+  }
 }
