ff8e072219ff42ecc31cb74c5b2f375945bfe4df
Roland Hummel <roland.hummel@qaware.de>
2020-03-22 16:55:08 +0100

    [FRONTEND] Re-styled login (and landing) page

--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface LoginFormValue {
+  email : string;
+  password : string;
+}
 
 @Component({
   selector: 'login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent {
 
   formController = new FormGroup({
     email: new FormControl('', [Validators.email, Validators.required]),
     password: new FormControl('', [Validators.required])
   });
 
-  constructor() { }
+  constructor () { }
 
-  ngOnInit(): void {
+  onSubmit () : void {
+    const formValue : LoginFormValue = this.formController.value;
+    console.log(formValue);
   }
 
-  onSubmit(): void {
-    const formValue: any = this.formController.value;
-  }
 }
