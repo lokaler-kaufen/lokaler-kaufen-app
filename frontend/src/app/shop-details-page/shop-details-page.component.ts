ab6ef10f1a38f700163d86b95a9beda275030e1e
Veronika Dashuber <veronika.dashuber@qaware.de>
2020-03-22 15:12:31 +0100

    [FRONTEND] Add dummy reservations in frontend

--- frontend/src/app/shop-details-page/shop-details-page.component.ts
+++ frontend/src/app/shop-details-page/shop-details-page.component.ts
@@ -40,6 +40,45 @@ export class ShopDetailsPageComponent implements OnInit {
   shopId: string;
   details: ShopDetailDto;
 
+  slots: Slot[] = [
+    {
+      id: '0',
+      start: '10:00',
+      end: '11:00',
+      available: false
+    },
+    {
+      id: '1',
+      start: '11:00',
+      end: '12:00',
+      available: false
+    },
+    {
+      id: '2',
+      start: '12:00',
+      end: '13:00',
+      available: false
+    },
+    {
+      id: '3',
+      start: '13:00',
+      end: '14:00',
+      available: true
+    },
+    {
+      id: '4',
+      start: '14:00',
+      end: '15:00',
+      available: true
+    },
+    {
+      id: '5',
+      start: '15:00',
+      end: '16:00',
+      available: true
+    }
+  ];
+
 
   constructor(private client: HttpClient,
               private activatedRoute: ActivatedRoute,
@@ -72,18 +111,6 @@ export class ShopDetailsPageComponent implements OnInit {
       .afterClosed()
       .subscribe((data: BookingPopupResult) => {
         if (data.outcome === BookingPopupOutcome.BOOK) {
-
-          console.warn('NOT IMPLEMENTED', data, id);
-          const registerSlotDto = {
-            shop: this.shopId,
-            slotId: id,
-            contactType: data.option,
-            contactNumber: data.phoneNumber,
-            customerName: data.name,
-            email: data.email
-          };
-          // TODO http call to the frontend api goes here
-
           const successConfig = new MatDialogConfig();
           successConfig.autoFocus = true;
           successConfig.width = '100%';
@@ -91,8 +118,8 @@ export class ShopDetailsPageComponent implements OnInit {
             owner: this.details.name,
             contactNumber: data.phoneNumber,
             contactType: data.option,
-            // start: this.details.slots.find(s => s.id === id).start,
-            // end: this.details.slots.find(s => s.id === id).end,
+            start: this.slots.find(s => s.id === id).start,
+            end: this.slots.find(s => s.id === id).end,
           } as BookingSuccessData;
 
           /*this.shopControllerService.getDetailsUsingGET(this.shopId)
