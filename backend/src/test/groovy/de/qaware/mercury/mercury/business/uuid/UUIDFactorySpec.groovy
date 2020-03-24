802ef7a68944859463750333b7036f59e17a5f1c
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-03-24 21:51:26 +0100

    Added more Spring tests fpr business util classes.

---
+++ backend/src/test/groovy/de/qaware/mercury/mercury/business/uuid/UUIDFactorySpec.groovy
@@ -0,0 +1,22 @@
+package de.qaware.mercury.mercury.business.uuid
+
+import de.qaware.mercury.mercury.business.uuid.impl.UUIDFactoryImpl
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+@ContextConfiguration(classes = UUIDFactoryImpl)
+class UUIDFactorySpec extends Specification {
+
+    @Autowired
+    UUIDFactory uuidFactory
+
+    def "Create UUI with factory"() {
+        given:
+        def uuid1 = uuidFactory.create()
+        def uuid2 = uuidFactory.create()
+
+        expect:
+        uuid1 != uuid2
+    }
+}
