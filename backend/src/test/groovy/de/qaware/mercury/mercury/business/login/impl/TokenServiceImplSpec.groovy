a68aa3219eac5107c4dbac699814cbfe0870cdaf
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-03-24 23:08:11 +0100

    Migrated missing method levels tests.

--- backend/src/test/groovy/de/qaware/mercury/mercury/business/login/impl/TokenServiceImplSpec.groovy
+++ backend/src/test/groovy/de/qaware/mercury/mercury/business/login/impl/TokenServiceImplSpec.groovy
@@ -1,8 +1,8 @@
 package de.qaware.mercury.mercury.business.login.impl
 
 import de.qaware.mercury.mercury.business.admin.Admin
-import de.qaware.mercury.mercury.business.login.AdminToken
-import de.qaware.mercury.mercury.business.login.TokenService
+import de.qaware.mercury.mercury.business.login.*
+import de.qaware.mercury.mercury.business.shop.Shop
 import spock.lang.Specification
 
 class TokenServiceImplSpec extends Specification {
@@ -25,5 +25,40 @@ class TokenServiceImplSpec extends Specification {
 
         then:
         idFromToken == id
+    }
+
+    def "Create and Verify Shop Token"() {
+        given:
+        ShopLogin.Id shopLoginId = ShopLogin.Id.of(UUID.randomUUID())
+        Shop.Id shopId = Shop.Id.of(UUID.randomUUID())
+        ShopToken token = tokenService.createShopToken(shopLoginId, shopId)
+
+        when:
+        ShopLogin.Id idFromToken = tokenService.verifyShopToken(token)
+
+        then:
+        idFromToken == shopLoginId
+    }
+
+    def "Create and Verify Shop Creation Token"() {
+        given:
+        ShopCreationToken token = tokenService.createShopCreationToken("foo@bar.org")
+
+        when:
+        String email = tokenService.verifyShopCreationToken(token)
+
+        then:
+        email == "foo@bar.org"
+    }
+
+    def "Create and Verify Password Reset Token"() {
+        given:
+        PasswordResetToken token = tokenService.createPasswordResetToken("foo@bar.org")
+
+        when:
+        String email = tokenService.verifyPasswordResetToken(token)
+
+        then:
+        email == "foo@bar.org"
     }
 }
