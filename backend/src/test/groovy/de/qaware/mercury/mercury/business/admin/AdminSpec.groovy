c680a6c5a15022d0dc68f95b92c07b6c015aa9ee
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-03-24 23:08:34 +0100

    Added Admin value object test spec.

---
+++ backend/src/test/groovy/de/qaware/mercury/mercury/business/admin/AdminSpec.groovy
@@ -0,0 +1,36 @@
+package de.qaware.mercury.mercury.business.admin
+
+import de.qaware.mercury.mercury.business.uuid.UUIDFactory
+import spock.lang.Specification
+
+class AdminSpec extends Specification {
+
+    def "Check random Admin.Id"() {
+        given:
+        def uuid = UUID.randomUUID()
+        def uuidFactory = Stub(UUIDFactory)
+        uuidFactory.create() >> uuid
+
+        when:
+        def random = Admin.Id.random(uuidFactory)
+
+        then:
+        random
+        random.id == uuid
+        random.id.toString() == uuid.toString()
+    }
+
+
+    def "Check parse Admin.Id"() {
+        given:
+        def uuid = UUID.randomUUID()
+
+        when:
+        def parsed = Admin.Id.parse(uuid.toString())
+
+        then:
+        parsed
+        parsed.id == uuid
+        parsed.id.toString() == uuid.toString()
+    }
+}
