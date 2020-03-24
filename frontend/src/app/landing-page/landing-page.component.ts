34aa4e08a58fa2bb0dc55574f899e0708360675f
Veronika Dashuber <veronika.dashuber@qaware.de>
2020-03-24 19:09:03 +0100

    [FRONTEND] Disable submit button for invalid zip codes

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {LocationControllerService, LocationSuggestionDto} from "../data/client";
-import {debounceTime, map, switchMap} from "rxjs/operators";
-import {Observable} from "rxjs";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {LocationControllerService, LocationSuggestionDto, LocationSuggestionsDto} from '../data/client';
+import {debounceTime, map, switchMap} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'landing-page',
@@ -11,31 +11,36 @@ import {Observable} from "rxjs";
 })
 export class LandingPageComponent implements OnInit {
   location: string;
-  suggestions: Observable<LocationSuggestionDto[]>;
+  suggestions: LocationSuggestionDto[] = [];
   form: FormGroup;
+  validZipCode = true;
 
   constructor(private formBuilder: FormBuilder,
-              private locationControllerService: LocationControllerService) {
+              private locationControllerService: LocationControllerService,
+              private client: HttpClient) {
   }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       zipCode: ['', [Validators.required, Validators.pattern(new RegExp(/^\d{5}$/))]]
     });
-    this.suggestions = this.form.controls.zipCode.valueChanges
+    // clear completion if no user input
+    this.form.controls.zipCode.valueChanges.subscribe(value => {
+      if (!value) {
+        this.suggestions = [];
+      }
+    });
+    this.form.controls.zipCode.valueChanges
       .pipe(
-        debounceTime(100),
+        debounceTime(300),
         switchMap(value => {
           if (value) {
-            return this.locationControllerService.getSuggestionsUsingGET(value)
-              .pipe(map((response) => response.locationSuggestions));
+            return this.client.get<LocationSuggestionsDto>('/api/location/suggestion?zipCode=' + value)
+              .pipe(map((response) => response.suggestions));
           } else {
             return [];
           }
-        }));
+        })).subscribe(suggestions => this.suggestions = suggestions);
   }
 
-  filter(values) {
-    console.log(values);
-  }
 }
