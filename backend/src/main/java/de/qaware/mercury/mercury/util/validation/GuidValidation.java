b56e04102cea8f70c9834220143cf5c3957228f0
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-03-25 22:48:03 +0100

    Added private constructor to util classes.

--- backend/src/main/java/de/qaware/mercury/mercury/util/validation/GuidValidation.java
+++ backend/src/main/java/de/qaware/mercury/mercury/util/validation/GuidValidation.java
@@ -1,5 +1,9 @@
 package de.qaware.mercury.mercury.util.validation;
 
-public class GuidValidation {
+public final class GuidValidation {
+
+    private GuidValidation() {
+    }
+
     public static final String REGEX = "^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$";
 }
