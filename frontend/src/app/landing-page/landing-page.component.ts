dcdbdc92352cd8c14c554e6c42fa4c6470fd3e6f
Dominik Haas <dominik.haas@qaware.de>
2020-03-25 22:07:46 +0100

    #39 Changed logic of start button: It is now enabled if valid ZIP code AND (value from cache unchanged OR there is at least one suggestion). This is closer to the original solution but doesn not validate previous entries from the cache. First they should be valid because they were added and second we don't do a server request to make it faster.

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -13,7 +13,7 @@ import {ZipCodeCacheService} from './zip-code-cache.service';
 })
 export class LandingPageComponent implements OnInit {
   location: string;
-  zipCode: string;
+  private zipCodeInitialValue: string;
   suggestions: LocationSuggestionDto[] = [];
   form: FormGroup;
 
@@ -25,9 +25,11 @@ export class LandingPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.zipCodeInitialValue = this.zipCodeCacheService.getZipCode();
+
     this.form = this.formBuilder.group({
       // pre-fill with value from cache (if present)
-      zipCode: [this.zipCodeCacheService.getZipCode(), [Validators.required, Validators.pattern(new RegExp(/^\d{5}$/))]]
+      zipCode: [this.zipCodeInitialValue, [Validators.required, Validators.pattern(new RegExp(/^\d{5}$/))]]
     });
 
     // clear completion if no user input
@@ -49,8 +51,36 @@ export class LandingPageComponent implements OnInit {
         })).subscribe(suggestions => this.suggestions = suggestions);
   }
 
+  public get startDisabled(): boolean {
+    return !this.startEnabled;
+  }
+
+  private get startEnabled(): boolean {
+    // form has to be valid (of course), if it is:
+    // - we allow to click start if the value was unchanged (this way the user can research fast)
+    //    or
+    // - if the changed zip code has hits
+    return this.form.valid && this.inputUnchangedOrHasSuggestionHits;
+  }
+
+  private get inputUnchangedOrHasSuggestionHits(): boolean {
+    return this.inputUnchanged || this.hasSuggestionHits;
+  }
+
+  private get inputUnchanged(): boolean {
+    return this.zipCodeInitialValue === this.zipCodeFromInput;
+  }
+
+  private get hasSuggestionHits(): boolean {
+    return this.suggestions.length > 0;
+  }
+
+  private get zipCodeFromInput(): string {
+    return this.form.controls.zipCode.value;
+  }
+
   public start(): void {
-    const locationFromInput = this.form.controls.zipCode.value;
+    const locationFromInput = this.zipCodeFromInput;
 
     // cache the zipCode for later
     this.zipCodeCacheService.setZipCode(locationFromInput);
