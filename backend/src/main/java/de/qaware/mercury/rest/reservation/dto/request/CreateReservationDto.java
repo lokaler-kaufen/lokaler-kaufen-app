84d6bdac963e8450517a53baf57e5d06acac13e5
Moritz Kammerer <moritz.kammerer@qaware.de>
2020-03-26 08:48:40 +0100

    Remove wrong pattern on slot id

--- backend/src/main/java/de/qaware/mercury/rest/reservation/dto/request/CreateReservationDto.java
+++ backend/src/main/java/de/qaware/mercury/rest/reservation/dto/request/CreateReservationDto.java
@@ -1,6 +1,5 @@
 package de.qaware.mercury.rest.reservation.dto.request;
 
-import de.qaware.mercury.util.validation.GuidValidation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,14 +7,12 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateReservationDto {
     @NotBlank
-    @Pattern(regexp = GuidValidation.REGEX)
     private String slotId;
     @NotBlank
     // TODO: Validate enum
