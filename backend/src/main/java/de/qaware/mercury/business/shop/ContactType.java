f93c3be2066199e94e39679491f494b994f67419
Moritz Kammerer <moritz.kammerer@qaware.de>
2020-03-27 18:32:55 +0100

    Add explicit id to the ContactType enum, which is used in REST and storage layer

--- backend/src/main/java/de/qaware/mercury/business/shop/ContactType.java
+++ backend/src/main/java/de/qaware/mercury/business/shop/ContactType.java
@@ -3,26 +3,32 @@ package de.qaware.mercury.business.shop;
 import lombok.Getter;
 
 public enum ContactType {
-    WHATSAPP("WhatsApp"),
-    PHONE("Telefon"),
-    FACETIME("FaceTime"),
-    GOOGLE_DUO("Google Duo"),
-    SKYPE("Skype"),
-    SIGNAL("Signal"),
-    VIBER("Viber");
+    WHATSAPP("WHATSAPP", "WhatsApp"),
+    PHONE("PHONE", "Telefon"),
+    FACETIME("FACETIME", "FaceTime"),
+    GOOGLE_DUO("GOOGLE_DUO", "Google Duo"),
+    SKYPE("SKYPE", "Skype"),
+    SIGNAL("SIGNAL", "Signal"),
+    VIBER("VIBER", "Viber");
+
+    @Getter
+    private final String id;
 
     @Getter
     private final String humanReadable;
 
-    ContactType(String humanReadable) {
+    ContactType(String id, String humanReadable) {
+        this.id = id;
         this.humanReadable = humanReadable;
     }
 
     public static ContactType parse(String input) {
-        try {
-            return valueOf(input);
-        } catch (IllegalArgumentException e) {
-            throw new InvalidContactTypeException(input, e);
+        for (ContactType contactType : ContactType.values()) {
+            if (contactType.getId().equals(input)) {
+                return contactType;
+            }
         }
+
+        throw new InvalidContactTypeException(input);
     }
 }
