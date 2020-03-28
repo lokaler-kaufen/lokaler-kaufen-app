d6b9801ad0bd789aec30811a089368926bd484ca
vlow <vlow+git@turbocache3000.de>
2020-03-28 07:27:55 +0100

    Throw exception if end is before start in SlotServiceImpl
    The slot calculation only works, if the end date is equal to or after the start date. As a consequence, an empty list has been returned. This could potentially trigger strange behavior within the application, if start and end date are mixed up (as happened in the broken test).
    
    To make this mistake obvious, I added an IllegalArgumentException if the end date is before the start date.
    
    I also changed the test to use LocalDate instead of LocalDateTime, to avoid further confusion with time stamps.

--- backend/src/main/java/de/qaware/mercury/business/reservation/SlotService.java
+++ backend/src/main/java/de/qaware/mercury/business/reservation/SlotService.java
@@ -10,9 +10,10 @@ public interface SlotService {
     /**
      * Generates a list of available time slots from a given slot config which are between the start and the end date.
      * If the start or end time of an existing reservation is within a time slot, it is omitted from the list.
-     * @param start The start date (date only, time is ignored)
-     * @param end The end date (date only, time is ignored)
-     * @param slotConfig The slot configuration
+     *
+     * @param start                The start date
+     * @param end                  The end date, must be after start
+     * @param slotConfig           The slot configuration
      * @param existingReservations Any existing reservations
      * @return A list of available time slots between the start and end date.
      */
