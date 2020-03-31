7c4b4de30f8ba449c1acbbd413ad2909fda1b2ea
Moritz Kammerer <moritz.kammerer@qaware.de>
2020-03-31 20:36:01 +0200

    Move @EnableScheduled in own config
    This eases testing and modular building of Spring context when testing

---
+++ backend/src/main/java/de/qaware/mercury/plumbing/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package de.qaware.mercury.plumbing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+class SchedulingConfig {
+}
