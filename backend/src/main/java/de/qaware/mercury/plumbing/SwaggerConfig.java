695c6e130437d3514a7173fb60aaf789fc7b41e4
Moritz Kammerer <moritz.kammerer@qaware.de>
2020-03-31 09:23:55 +0200

    Add Swagger documentation only when running in `dev` profile

--- backend/src/main/java/de/qaware/mercury/plumbing/SwaggerConfig.java
+++ backend/src/main/java/de/qaware/mercury/plumbing/SwaggerConfig.java
@@ -2,6 +2,7 @@ package de.qaware.mercury.plumbing;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Profile("dev")
 class SwaggerConfig {
     @Bean
     Docket api() {
