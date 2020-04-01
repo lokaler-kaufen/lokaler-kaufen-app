f05d530f28fa91f9d660a1150a2327994534f9f3
Dominik Haas <dominik.haas@qaware.de>
2020-04-01 21:52:18 +0200

    #205 Added unit test for back functionality

--- frontend/src/app/shop-details-page/shop-details-page.component.spec.ts
+++ frontend/src/app/shop-details-page/shop-details-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShopDetailsPageComponent } from './shop-details-page.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MatDialogModule} from '@angular/material/dialog';
+import {SimpleNotificationsModule} from 'angular2-notifications';
 
 describe('ShopDetailsPageComponent', () => {
   let component: ShopDetailsPageComponent;
@@ -8,6 +12,7 @@ describe('ShopDetailsPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule, MatDialogModule, SimpleNotificationsModule.forRoot()],
       declarations: [ ShopDetailsPageComponent ]
     })
     .compileComponents();
@@ -21,5 +26,13 @@ describe('ShopDetailsPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate back when method back is called', () => {
+    spyOn(window.history, 'back');
+
+    component.back();
+
+    expect(window.history.back).toHaveBeenCalled();
   });
 });
