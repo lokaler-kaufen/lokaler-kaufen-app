c74dd39ab4b17a80b4b0bef699aebcded66b52bc
Veronika Dashuber <veronika.dashuber@qaware.de>
2020-04-02 22:33:56 +0200

    Add logout link to footer #207

--- frontend/src/app/app-footer/app-footer.component.ts
+++ frontend/src/app/app-footer/app-footer.component.ts
@@ -3,49 +3,63 @@ import {MatDialog} from '@angular/material/dialog';
 import {RegisterBusinessPopupComponent} from '../register-business-popup/register-business-popup.component';
 import {UserContextService} from '../shared/user-context.service';
 import {HttpClient} from '@angular/common/http';
+import {NotificationsService} from 'angular2-notifications';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './app-footer.component.html',
-  styleUrls: ['./app-footer.component.css']
+    selector: 'app-footer',
+    templateUrl: './app-footer.component.html',
+    styleUrls: ['./app-footer.component.css']
 })
 export class AppFooterComponent implements OnInit {
 
-  // hold the dialog ref as long as the dialog is open
-  dialogRef;
+    // hold the dialog ref as long as the dialog is open
+    dialogRef;
 
-  // version info
-  versionInfo: VersionInfo;
+    // version info
+    versionInfo: VersionInfo;
 
-  constructor(public dialog: MatDialog, private client: HttpClient, public userContextService: UserContextService) {
-  }
-
-  openRegisterBusinessPopup(): void {
-    if (!this.dialogRef) {
-      this.dialogRef = this.dialog.open(RegisterBusinessPopupComponent, {
-        width: '500px'
-      });
-
-      this.dialogRef.afterClosed().subscribe(
-        () => {
-          // Reset the dialogRef because the user should be able to open the dialog again.
-          this.dialogRef = null;
-        }
-      );
+    constructor(public dialog: MatDialog,
+                private client: HttpClient,
+                public userContextService: UserContextService,
+                private notificationsService: NotificationsService) {
     }
-  }
 
-  ngOnInit(): void {
-    this.client.get<VersionInfo>('/api/info/version').toPromise().then(info => {
-      this.versionInfo = info;
-    });
-  }
+    openRegisterBusinessPopup(): void {
+        if (!this.dialogRef) {
+            this.dialogRef = this.dialog.open(RegisterBusinessPopupComponent, {
+                width: '500px'
+            });
 
+            this.dialogRef.afterClosed().subscribe(
+                () => {
+                    // Reset the dialogRef because the user should be able to open the dialog again.
+                    this.dialogRef = null;
+                }
+            );
+        }
+    }
+
+    ngOnInit(): void {
+        this.client.get<VersionInfo>('/api/info/version').toPromise().then(info => {
+            this.versionInfo = info;
+        });
+    }
+
+    logoutStoreOwner() {
+        this.client.post('/api/shop/login/logout', {}).toPromise().then(() => {
+            console.log('Shop Owner logged out. ');
+            this.userContextService.storeOwnerLoggedOut();
+        }).catch(error => {
+            console.log('Unable to logout shop owner: ' + error.status + ' ' + error.message);
+            this.notificationsService.error('Tut uns Leid!', 'Beim Logout ist etwas schiefgegangen.');
+        });
+
+    }
 }
 
 export interface VersionInfo {
-  commitHash: string;
-  commitTime: string;
-  localChanges: string;
-  version: string;
+    commitHash: string;
+    commitTime: string;
+    localChanges: string;
+    version: string;
 }
