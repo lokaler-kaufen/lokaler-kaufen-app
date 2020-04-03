97828923cb470d6173d85da19eff9f8042cd4cac
Moritz Kammerer <moritz.kammerer@qaware.de>
2020-04-03 07:49:18 +0200

    [#207] Change API to logout and fix Expires header in cookie

--- backend/src/main/java/de/qaware/mercury/rest/login/ShopLoginController.java
+++ backend/src/main/java/de/qaware/mercury/rest/login/ShopLoginController.java
@@ -13,6 +13,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,7 +42,7 @@ class ShopLoginController {
         cookieHelper.addTokenCookie(SHOP_COOKIE_NAME, token, response);
     }
 
-    @PostMapping("/logout")
+    @DeleteMapping
     public void logout(HttpServletResponse response) {
         cookieHelper.removeCookie(SHOP_COOKIE_NAME, response);
     }
