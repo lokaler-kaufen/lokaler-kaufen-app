f7904d987d212c9a0425d787da69696121bc9cf6
Roland Hummel <roland.hummel@qaware.de>
2020-04-03 23:03:11 +0200

    Clean up AppComponent code a bit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,36 +2,29 @@ import {Component} from '@angular/core';
 import {MatIconRegistry} from '@angular/material/icon';
 import {DomSanitizer} from '@angular/platform-browser';
 
+const additionalIcons: Array<{ id: string, asset: string; }> = [
+  {id: 'WHATSAPP', asset: '../assets/whatsapp.svg'},
+  {id: 'FACETIME', asset: '../assets/voice_chat.svg'},
+  {id: 'PHONE', asset: '../assets/phone.svg'},
+  {id: 'GOOGLE_DUO', asset: '../assets/google_duo.svg'},
+  {id: 'SKYPE', asset: '../assets/skype.svg'},
+  {id: 'VIBER', asset: '../assets/viber.svg'},
+  {id: 'SIGNAL', asset: '../assets/signal.svg'}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
-    this.matIconRegistry.addSvgIcon(
-      'WHATSAPP',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/whatsapp.svg'))
-      .addSvgIcon(
-        'FACETIME',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/voice_chat.svg'))
-      .addSvgIcon(
-        'PHONE',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/phone.svg'))
-      .addSvgIcon(
-        'GOOGLE_DUO',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/google_duo.svg'))
-      .addSvgIcon(
-        'SKYPE',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/skype.svg'))
-      .addSvgIcon(
-        'VIBER',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/viber.svg'))
-      .addSvgIcon(
-        'SIGNAL',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/signal.svg')
-      );
+    additionalIcons.forEach(({asset, id}) => {
+      this.matIconRegistry.addSvgIcon(id, this.domSanitizer.bypassSecurityTrustResourceUrl(asset));
+    });
   }
 
-  isIEOrEdge = /msie\s|trident\/|edge\//i.test(window.navigator.userAgent)
+  isIEOrEdge = /msie\s|trident\/|edge\//i.test(window.navigator.userAgent);
+
 }
