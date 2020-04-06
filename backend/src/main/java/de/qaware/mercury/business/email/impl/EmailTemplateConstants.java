7ca96258024d1d595e8c44ded5f9000a6fa05944
vlow <vlow+git@turbocache3000.de>
2020-04-06 21:17:13 +0200

    Fix order of modifiers and hide constructor.

--- backend/src/main/java/de/qaware/mercury/business/email/impl/EmailTemplateConstants.java
+++ backend/src/main/java/de/qaware/mercury/business/email/impl/EmailTemplateConstants.java
@@ -5,27 +5,31 @@ package de.qaware.mercury.business.email.impl;
  */
 public class EmailTemplateConstants {
 
-    public final static String TOKEN = "{{ token }}";
-    public final static String SHOP_NAME = "{{ shopName }}";
-    public final static String CUSTOMER_NAME = "{{ customerName }}";
-    public final static String OWNER_NAME = "{{ ownerName }}";
-    public final static String CONTACT_TYPE = "{{ contactType }}";
-    public final static String SLOT_DATE = "{{ date }}";
-    public final static String SLOT_START_TIME = "{{ slotStart }}";
-    public final static String SLOT_END_TIME = "{{ slotEndTime }}";
-    public final static String CONTACT = "{{ contact }}";
-    public final static String SHOP_CREATION_LINK = "{{ link }}";
-    public final static String CANCEL_RESERVATION_LINK = "{{ cancelReservationLink }}";
-    public final static String PASSWORD_RESET_LINK = "{{ link }}";
+    private EmailTemplateConstants() {
+        // hide constructor; no instances needed.
+    }
 
-    public final static String SHOP_EMAIL_ADDRESS = "{{ email }}";
-    public final static String SHOP_STREET = "{{ street }}";
-    public final static String SHOP_ZIP_CODE = "{{ zipCode }}";
-    public final static String SHOP_CITY = "{{ city }}";
-    public final static String SHOP_ADDRESS_SUPPLEMENT = "{{ addressSupplement }}";
-    public final static String SHOP_WEBSITE = "{{ addressSupplement }}";
-    public final static String SHOP_DETAILS = "{{ details }}";
+    public static final String TOKEN = "{{ token }}";
+    public static final String SHOP_NAME = "{{ shopName }}";
+    public static final String CUSTOMER_NAME = "{{ customerName }}";
+    public static final String OWNER_NAME = "{{ ownerName }}";
+    public static final String CONTACT_TYPE = "{{ contactType }}";
+    public static final String SLOT_DATE = "{{ date }}";
+    public static final String SLOT_START_TIME = "{{ slotStart }}";
+    public static final String SLOT_END_TIME = "{{ slotEndTime }}";
+    public static final String CONTACT = "{{ contact }}";
+    public static final String SHOP_CREATION_LINK = "{{ link }}";
+    public static final String CANCEL_RESERVATION_LINK = "{{ cancelReservationLink }}";
+    public static final String PASSWORD_RESET_LINK = "{{ link }}";
 
-    public final static String ADMIN_UI_LINK = "{{ adminUiLink}}";
+    public static final String SHOP_EMAIL_ADDRESS = "{{ email }}";
+    public static final String SHOP_STREET = "{{ street }}";
+    public static final String SHOP_ZIP_CODE = "{{ zipCode }}";
+    public static final String SHOP_CITY = "{{ city }}";
+    public static final String SHOP_ADDRESS_SUPPLEMENT = "{{ addressSupplement }}";
+    public static final String SHOP_WEBSITE = "{{ addressSupplement }}";
+    public static final String SHOP_DETAILS = "{{ details }}";
+
+    public static final String ADMIN_UI_LINK = "{{ adminUiLink}}";
 
 }
