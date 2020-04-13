b41eff599e1d5fc342037b94124c119919677186
Franz Wimmer <franz.wimmer@qaware.de>
2020-04-13 14:47:49 +0200

    [#176] Apply UTF-8 encoding to loaded text snippets

--- backend/src/main/java/de/qaware/mercury/plumbing/I18nConfig.java
+++ backend/src/main/java/de/qaware/mercury/plumbing/I18nConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 @Configuration
@@ -24,6 +25,7 @@ public class I18nConfig {
     public ResourceBundleMessageSource messageSource() {
         ResourceBundleMessageSource source = new ResourceBundleMessageSource();
         source.setBasenames("i18n/messages");
+        source.setDefaultEncoding(StandardCharsets.UTF_8.toString());
         return source;
     }
 }
