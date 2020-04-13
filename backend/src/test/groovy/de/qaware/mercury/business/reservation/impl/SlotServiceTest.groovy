c0fc5658ff3fa182938b8ecc25a5bb98bcf885b0
Moritz Kammerer <kammerer.moritz@gmail.com>
2020-04-13 13:21:23 +0200

    [#225] Add unit test for breaks / slots conversion

--- backend/src/test/groovy/de/qaware/mercury/business/reservation/impl/SlotServiceTest.groovy
+++ backend/src/test/groovy/de/qaware/mercury/business/reservation/impl/SlotServiceTest.groovy
@@ -3,14 +3,14 @@ package de.qaware.mercury.business.reservation.impl
 import de.qaware.mercury.business.reservation.Interval
 import de.qaware.mercury.business.reservation.Slot
 import de.qaware.mercury.business.reservation.SlotService
+import de.qaware.mercury.business.reservation.Slots
+import de.qaware.mercury.business.shop.Breaks
 import de.qaware.mercury.business.shop.DayConfig
 import de.qaware.mercury.business.shop.SlotConfig
 import de.qaware.mercury.business.time.Clock
 import de.qaware.mercury.test.builder.SlotConfigBuilder
-import org.spockframework.spring.SpringBean
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
+import spock.lang.Subject
 import spock.lang.Unroll
 
 import java.time.DayOfWeek
@@ -21,14 +21,11 @@ import java.time.temporal.TemporalAdjusters
 
 import static java.time.LocalDateTime.now
 
-@ContextConfiguration(classes = SlotServiceImpl)
 class SlotServiceTest extends Specification {
-
-    @Autowired
-    SlotService slotService
-
-    @SpringBean
     Clock clock = Mock()
+
+    @Subject
+    SlotService slotService = new SlotServiceImpl(clock)
 
     @Unroll
     def "Check Empty Slot Generation: #usecase"() {
@@ -86,6 +83,45 @@ class SlotServiceTest extends Specification {
         slotService.generateSlots(sunday(), saturday(), saturdayConfig(8, 18), new ArrayList<Interval>())
         then:
         IllegalArgumentException _ = thrown()
+    }
+
+    def "test breaks"() {
+        given: "a fixed date"
+        clock.now() >> LocalDateTime.parse("2020-04-13T13:09:39.940482")
+        SlotConfig slotConfig = mondayConfig(10, 12);
+
+        when: "we preview the slots"
+        Slots preview = slotService.previewSlots(slotConfig)
+
+        then: "we get two slots, starting next monday"
+        // Next monday (see clock above)
+        preview.begin == LocalDate.of(2020, 4, 20)
+        preview.days == 7
+        // 10:00 - 10:30
+        preview.slots[0].start == LocalDateTime.of(2020, 4, 20, 10, 0)
+        preview.slots[0].end == LocalDateTime.of(2020, 4, 20, 10, 30)
+        // 11:00 - 11:30
+        preview.slots[1].start == LocalDateTime.of(2020, 4, 20, 11, 0)
+        preview.slots[1].end == LocalDateTime.of(2020, 4, 20, 11, 30)
+
+        when: "we resolve these slots to breaks"
+        Breaks breaks = slotService.resolveBreaks(Set.of(preview.slots[0].id, preview.slots[1].id), slotConfig)
+
+        then: "we get breaks containing exactly the both slots"
+        breaks.getMonday() == Set.of(
+            Breaks.Break.of(LocalTime.of(10, 0), LocalTime.of(10, 30)), // Slot 1
+            Breaks.Break.of(LocalTime.of(11, 0), LocalTime.of(11, 30)) // Slot 2
+        )
+
+        when: "we convert the breaks back to slots"
+        List<Slot> slots = slotService.convertBreaksToSlots(breaks)
+
+        then: "we get exactly these slots back"
+        slots.size() == 2
+        slots[0].start == LocalDateTime.of(2020, 4, 20, 10, 0)
+        slots[0].end == LocalDateTime.of(2020, 4, 20, 10, 30)
+        slots[1].start == LocalDateTime.of(2020, 4, 20, 11, 0)
+        slots[1].end == LocalDateTime.of(2020, 4, 20, 11, 30)
     }
 
     static LocalDate monday() {
