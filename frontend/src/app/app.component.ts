fa47d5309920285c6b1b5c7af02a595c8f454cb6
Franz Wimmer <franz.wimmer@qaware.de>
2020-04-13 14:48:23 +0200

    [#176] Extract texts out of html templates

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Injector, OnInit} from '@angular/core';
 import {MatIconRegistry} from '@angular/material/icon';
 import {DomSanitizer} from '@angular/platform-browser';
 import {AdminService} from './shared/admin.service';
