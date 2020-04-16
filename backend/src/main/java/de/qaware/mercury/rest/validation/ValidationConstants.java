bff4b9b58b9b5284d2e2a8e82ae0007e2609674e
Moritz Kammerer <moritz.kammerer@qaware.de>
2020-04-16 15:56:51 +0200

    Fix comment

--- backend/src/main/java/de/qaware/mercury/rest/validation/ValidationConstants.java
+++ backend/src/main/java/de/qaware/mercury/rest/validation/ValidationConstants.java
@@ -5,7 +5,7 @@ public final class ValidationConstants {
     public static final int MAX_PASSWORD_LENGTH = 70;
 
     /**
-     * You have to enable {@link javax.validation.constraints.Pattern.Flag#CASE_INSENSITIVE} when using this regex.
+     * You have to enable {@link java.util.regex.Pattern#CASE_INSENSITIVE} when using this regex.
      */
     // From https://www.regular-expressions.info/email.html
     public static final String EMAIL_REGEX = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$";
