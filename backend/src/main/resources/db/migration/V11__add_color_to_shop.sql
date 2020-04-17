bebb47ec0fd962c4fd550f43a43d464ef7a97c75
vlow <vlow+git@turbocache3000.de>
2020-04-17 16:56:05 +0200

    Fix migration script

--- backend/src/main/resources/db/migration/V11__add_color_to_shop.sql
+++ backend/src/main/resources/db/migration/V11__add_color_to_shop.sql
@@ -1,5 +1,9 @@
 -- Add a shop auto color column and a switch to disable it
 ALTER TABLE shop
-    ADD COLUMN shop_color varchar DEFAULT "#FFFFFF";
-    ADD COLUMN auto_color_enabled boolean NOT NULL DEFAULT false;
+    ADD COLUMN shop_color varchar;
 
+ALTER TABLE shop
+    ADD COLUMN auto_color_enabled boolean NOT NULL DEFAULT FALSE;
+
+ALTER TABLE shop
+    ALTER COLUMN shop_color DROP DEFAULT;
