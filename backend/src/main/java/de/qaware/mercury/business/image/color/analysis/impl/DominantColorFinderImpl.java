88381b740ca205fa551936bd72089c8995ca350d
vlow <vlow+git@turbocache3000.de>
2020-04-19 18:40:27 +0200

    Add test for dominant color finder and throw a more specific exception if map is empty

--- backend/src/main/java/de/qaware/mercury/business/image/color/analysis/impl/DominantColorFinderImpl.java
+++ backend/src/main/java/de/qaware/mercury/business/image/color/analysis/impl/DominantColorFinderImpl.java
@@ -1,10 +1,12 @@
 package de.qaware.mercury.business.image.color.analysis.impl;
 
+import de.qaware.mercury.business.image.ImageException;
 import de.qaware.mercury.business.image.color.Color;
 import de.qaware.mercury.business.image.color.analysis.DominantColorFinder;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * A very simple implementation of the color histogram analyzer.
@@ -16,10 +18,14 @@ public class DominantColorFinderImpl implements DominantColorFinder {
 
     @Override
     public Color findDominantColor(Map<Color, Integer> colorHistogram) {
-        Map.Entry<Color, Integer> topColorEntry = colorHistogram.entrySet().stream()
-            .max(Map.Entry.comparingByValue())
-            .orElseThrow();
-        return topColorEntry.getKey();
+        Optional<Map.Entry<Color, Integer>> topColorEntry = colorHistogram.entrySet().stream()
+            .max(Map.Entry.comparingByValue());
+
+        if (topColorEntry.isEmpty()) {
+            throw new ImageException("Cannot find dominant color.");
+        }
+
+        return topColorEntry.get().getKey();
     }
 
 }
