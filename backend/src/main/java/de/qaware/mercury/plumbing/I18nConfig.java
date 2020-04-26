82de38dd90030dcf63b95f4c88228ac0eca4423f
Moritz Kammerer <kammerer.moritz@gmail.com>
2020-04-26 16:03:00 +0200

    Use FixedLocaleResolver

--- backend/src/main/java/de/qaware/mercury/plumbing/I18nConfig.java
+++ backend/src/main/java/de/qaware/mercury/plumbing/I18nConfig.java
@@ -4,21 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 @Configuration
 public class I18nConfig {
-
-    private static final Locale DEFAULT_LOCALE = Locale.GERMANY;
-
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver resolver = new SessionLocaleResolver();
-        resolver.setDefaultLocale(DEFAULT_LOCALE);
-        return resolver;
+        return new FixedLocaleResolver(Locale.GERMANY);
     }
 
     @Bean
