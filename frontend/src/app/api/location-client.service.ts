3f75035581138aab35c66135fb58f3c961cebe8e
Roland Hummel <roland.hummel@qaware.de>
2020-04-27 21:23:42 +0200

    Implement LocationClient
    Relates to #259

---
+++ frontend/src/app/api/location-client.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {ApiClientBase} from './api-client-base.service';
+import {LocationSuggestionsDto} from '../data/client/model/locationSuggestionsDto';
+import {Observable} from 'rxjs';
+
+const API = '/api/location';
+
+@Injectable({providedIn: 'root'})
+export class LocationClient {
+
+  constructor(private http: HttpClient, private base: ApiClientBase) {
+  }
+
+  getSuggestions(zipCode: string): Promise<LocationSuggestionsDto> {
+    return this.base.promisify(
+      this.http.get(`${API}/suggestion`, this.getOptions(zipCode)),
+      '[LocationClient] failed to retrieve suggestions'
+    );
+  }
+
+  isLocationKnown(zipCode: string): Promise<void> {
+    return this.base.promisify<void>(
+      this.http.get(API, this.getOptions(zipCode)) as Observable<any>,
+      '[LocationClient] failed to check known location'
+    );
+  }
+
+  private getOptions(zipCode: string) {
+    return {
+      params: new HttpParams().set('zipCode', zipCode)
+    };
+  }
+
+}
